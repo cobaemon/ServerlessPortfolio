1a5cbb66e7052769ed983487c435edc10668e03f
cobaemon <59654193+cobaemon@users.noreply.github.com>
2025-06-18 01:38:46 +0900

    Add WhiteNoise middleware and configuration for static file handling in base.py
    This update integrates WhiteNoise into the Django settings, enhancing static file management by adding the necessary middleware and storage backend. This change improves the application's ability to serve static files efficiently in production.

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
