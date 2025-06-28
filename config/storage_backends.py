78701809ac3977b78137a8867723c7f4e8c5da05
cobaemon <59654193+cobaemon@users.noreply.github.com>
2025-06-28 20:32:39 +0900

    Normalize paths in staticfiles manifest

--- config/storage_backends.py
+++ config/storage_backends.py
@@ -5,6 +5,13 @@ from django.core.files.storage import FileSystemStorage
 from storages.backends.s3 import S3ManifestStaticStorage
 
 
+def _normalize_manifest_paths(manifest):
+    """Return a new manifest with normalized path separators."""
+    if not manifest:
+        return {}
+    return {k.replace("\\", "/"): v.replace("\\", "/") for k, v in manifest.items()}
+
+
 class LocalManifestS3Storage(S3ManifestStaticStorage):
     """S3 storage that reads the manifest from the packaged staticfiles directory."""
 
@@ -12,3 +19,7 @@ class LocalManifestS3Storage(S3ManifestStaticStorage):
         manifest_location = os.path.join(settings.BASE_DIR, 'staticfiles')
         kwargs.setdefault('manifest_storage', FileSystemStorage(location=manifest_location))
         super().__init__(*args, **kwargs)
+
+    def load_manifest(self):
+        files, manifest_hash = super().load_manifest()
+        return _normalize_manifest_paths(files), manifest_hash
